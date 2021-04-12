df25605b56ba33f0f867b0c54922aba0887ce081
GianlucaBeltran <gianlucabeltran@gmail.com>
2021-04-12 16:35:41 -0500

    Added second level

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startPos;
+    private float startPos;
     public GameObject cam;
     public float parallaxEffect;
-
-
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
